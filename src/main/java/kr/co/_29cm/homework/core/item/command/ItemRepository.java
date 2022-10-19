7a8c7a6ae6c65526a8fa25294c5737052f9f2952
최선혁 <wafy75@gmail.com>
2022-10-19 12:53:18 +0900

    @Repository 어노테이션 추가

--- src/main/java/kr/co/_29cm/homework/core/item/command/ItemRepository.java
+++ src/main/java/kr/co/_29cm/homework/core/item/command/ItemRepository.java
@@ -2,7 +2,9 @@ package kr.co._29cm.homework.core.item.command;
 
 import kr.co._29cm.homework.core.item.Item;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends CrudRepository<Item, Long> {
 
     Item save(Item item);
