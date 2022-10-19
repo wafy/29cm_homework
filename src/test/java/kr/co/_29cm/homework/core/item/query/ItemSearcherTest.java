41dfbea24e6e0d9e5c36a5efebd4ac141f2ff2fc
최선혁 <wafy75@gmail.com>
2022-10-19 10:55:12 +0900

    상품 조회 테스트 코드 추가

--- src/test/java/kr/co/_29cm/homework/core/item/query/ItemSearcherTest.java
+++ src/test/java/kr/co/_29cm/homework/core/item/query/ItemSearcherTest.java
@@ -1,7 +1,40 @@
 package kr.co._29cm.homework.core.item.query;
 
-import static org.junit.jupiter.api.Assertions.*;
+import kr.co._29cm.homework.TestSupplier;
+import kr.co._29cm.homework.core.item.Item;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-class ItemSearcherTest {
+import java.util.List;
+
+@DisplayName("ItemSearcher class")
+@DataJpaTest
+class ItemSearcherTest extends TestSupplier {
+
+    @Nested
+    @DisplayName("findAll 메서드는")
+    class Describe_findAll_method {
+
+        @Nested
+        @DisplayName("조회 요청이면")
+        class Context_request_find {
+
+            @BeforeEach
+            void init() {
+                getItemCreator().create();
+            }
+
+            @Test
+            @DisplayName("등록된 데이터 19개 전체를 반환한다")
+            void it_return_all_data() {
+                List<Item> result = getItemSearcher().findAll();
+                Assertions.assertThat(result.size()).isEqualTo(19);
+            }
+        }
+    }
 
 }
